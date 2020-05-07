5b121b9968ae1738ff803d75e6748897d0574a43
DiegoTKD94 <diego.chavarriap@gmail.com>
2020-05-06 23:06:12 -0500

    Punto 4 ok

---
+++ Punto4FechaNacimiento.sql
@@ -0,0 +1,12 @@
+DECLARE
+    w_fecha_nacimiento DATE;
+    w_fecha_actual DATE;
+    w_meses_entre NUMBER;
+    w_edad NUMBER;
+BEGIN
+    w_fecha_nacimiento := '01/11/1994';
+    select CURRENT_TIMESTAMP into w_fecha_actual from dual;
+    w_meses_entre := MONTHS_BETWEEN(w_fecha_actual, w_fecha_nacimiento);
+    w_edad := TRUNC(w_meses_entre / 12);
+    dbms_output.put_line(w_edad);
+END;
